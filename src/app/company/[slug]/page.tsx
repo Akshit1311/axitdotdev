3780cb0a1adfac8b23a033bfc856627d3acdb19b
Akshit1311 <a4748g@gmail.com>
2026-03-06 14:41:07 +0700

    fix: next version

--- src/app/company/[slug]/page.tsx
+++ src/app/company/[slug]/page.tsx
@@ -4,47 +4,47 @@ import { COMPANIES, type TCompany } from "~/app/_constants/projects";
 import WorkExpPage from "~/app/_sections/WorkExpPage";
 
 export async function generateMetadata({
-	params,
+  params,
 }: {
-	params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
-	const { slug } = await params;
+  const { slug } = await params;
 
-	if (slug === "huddle") {
-		return {
-			openGraph: {
-				images: ["/assets/og/huddle01.png"],
-			},
-		};
-	}
+  if (slug === "huddle") {
+    return {
+      openGraph: {
+        images: ["/assets/og/huddle01.png"],
+      },
+    };
+  }
 
-	return {};
+  return {};
 }
 
 export default async function Page({
-	params,
+  params,
 }: {
-	params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
 }) {
-	const { slug } = await params;
+  const { slug } = await params;
 
-	if (!Object.keys(COMPANIES).includes(slug)) {
-		return redirect("/not-found");
-	}
+  if (!Object.keys(COMPANIES).includes(slug)) {
+    return redirect("/not-found");
+  }
 
-	console.log({ slug });
+  console.log({ slug });
 
-	return (
-		<div className="mx-auto">
-			<WorkExpPage companyKey={slug as TCompany} />
-		</div>
-	);
+  return (
+    <div className="mx-auto">
+      <WorkExpPage companyKey={slug as TCompany} />
+    </div>
+  );
 }
 
 export async function generateStaticParams() {
-	const paths = Object.keys(COMPANIES).map((company) => ({
-		slug: company,
-	}));
+  const paths = Object.keys(COMPANIES).map((company) => ({
+    slug: company,
+  }));
 
-	return paths;
+  return paths;
 }
